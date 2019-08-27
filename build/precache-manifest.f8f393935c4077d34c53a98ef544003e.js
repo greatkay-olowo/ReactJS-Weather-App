d6d0999d58aea44d2ac63ffd02ba1cbff051133f
Kayode Alao <Oluwakayode.alao@gmail.com>
2019-08-27 13:27:41 +0100

    Add files via upload

---
+++ build/precache-manifest.f8f393935c4077d34c53a98ef544003e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c303e580b5192f0958dae94fd8f599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d21ccf86e737330420e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "33e9e176cd38b2592a19",
+    "url": "/static/js/2.46c753d0.chunk.js"
+  },
+  {
+    "revision": "1d21ccf86e737330420e",
+    "url": "/static/js/main.de0cbed1.chunk.js"
+  },
+  {
+    "revision": "314928a80bef428b9533",
+    "url": "/static/js/runtime~main.7c976dd7.js"
+  }
+]);
